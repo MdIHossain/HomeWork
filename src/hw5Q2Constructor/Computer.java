b8162cfdf606bba540e0cee8d25b4808308620ed
MdIHossain <mdiyhossain@gmail.com>
2022-01-19 18:42:09 -0500

    01192022 6:41pm

--- src/hw5Q2Constructor/Computer.java
+++ src/hw5Q2Constructor/Computer.java
@@ -1,5 +1,7 @@
 package hw5Q2Constructor;
 
+import hw2Q2JavaBasics.MyCar;
+
 public class Computer {
 	public String brand;
 	public String model;
@@ -7,42 +9,48 @@ public class Computer {
 	public String price;
 	public char grade;
 	public boolean madeInUSA;
-	
+
 	public Computer() {
 		System.out.println("This is default constructor from Computer class");
 	}
+
 	public Computer(String brand) {
 		this.brand = brand;
-		System.out.println("My Brand : " +brand);
-		
+		System.out.println("My Brand : " + brand);
+
 	}
-	public Computer(String brand,String model) {
+
+	public Computer(String brand, String model) {
 		this.model = model;
-		System.out.println("Model  : " +model);
-		
+		System.out.println("My Brand " + brand + "Model  : " + model);
+
 	}
-	public Computer(String brand,String model,String operatingSystem ) {
+
+	public Computer(String brand, String model, String operatingSystem) {
 		this.operatingSystem = operatingSystem;
-		System.out.println("operatingSystem  : " +operatingSystem);
-		
+		System.out.println("My Brand " + brand + "Model  : " + model + "operatingSystem  : " + operatingSystem);
+
 	}
-	public Computer(String brand,String model,String operatingSystem,String price ) {
+
+	public Computer(String brand, String model, String operatingSystem, String price) {
 		this.price = price;
-		System.out.println("Price: "+ price);
-		
+		System.out.println("My Brand " + brand + "Model  : " + model + "operatingSystem  : " + operatingSystem
+				+ "Price: " + price);
+
 	}
-	public Computer(String brand,String model,String operatingSystem,String price,char grade ) {
+
+	public Computer(String brand, String model, String operatingSystem, String price, char grade) {
 		this.grade = grade;
-		System.out.println("Grade: "+ grade);
-		
+		System.out.println("My Brand " + brand + "Model  : " + model + "operatingSystem  : " + operatingSystem
+				+ "Price: " + price + "Grade: " + grade);
+
 	}
-	public Computer(String brand,String model,String operatingSystem,String price,char grade,boolean madeInUSA ) {
+
+	public Computer(String brand, String model, String operatingSystem, String price, char grade, boolean madeInUSA) {
 		this.madeInUSA = madeInUSA;
-		System.out.println("Made in USA? "+"Ans: " +madeInUSA );
-		
+		System.out.println("My Brand " + brand + "Model  : " + model + "operatingSystem  : " + operatingSystem
+				+ "Price: " + price + "Grade: " + grade + "Made in USA? " + "Ans: " + madeInUSA);
+
 	}
-	
-	
-	
 
 }
