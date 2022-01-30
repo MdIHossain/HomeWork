8e6a03dcc67f95858a30eca34eb68fe2248525e4
MdIHossain <mdiyhossain@gmail.com>
2022-01-30 10:03:18 -0500

    01302022 10:02am

--- src/hw6Q21IfElseCondition/Voter.java
+++ src/hw6Q21IfElseCondition/Voter.java
@@ -3,18 +3,17 @@ package hw6Q21IfElseCondition;
 public class Voter {
 
 	public static void main(String[] args) {
-		// here define a variable
-		int age;
 		// initialize a variable
-		int age1 = 38;
-		if (age1 == 18) {
-			System.out.println(" I am a voter" + age1);
-		} else if (age1 < 18) {
-			System.out.println(" I am not a voter " + age1);
-		} else if (age1 >= 18) {
-			System.out.println(" I am a voter " + age1);
+		int age = 38;
+		
+		if (age == 18) {
+			System.out.println(" I am a voter");
+		} else if (age < 18) {
+			System.out.println(" I am not a voter");
+		} else if (age > 18) {
+			System.out.println(" I am a voter");
 		} else {
-			System.out.println(age1 + "My age is 38");
+			System.out.println("Please add a valid age");
 		}
 
 	}
