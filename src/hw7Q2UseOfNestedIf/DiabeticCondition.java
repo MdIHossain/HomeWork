3be07f0e1b8ed55fec2bce06e4476a5c30ade51a
MdIHossain <mdiyhossain@gmail.com>
2022-02-01 23:27:21 -0500

    02012022 11:27pm

--- src/hw7Q2UseOfNestedIf/DiabeticCondition.java
+++ src/hw7Q2UseOfNestedIf/DiabeticCondition.java
@@ -7,16 +7,15 @@ public class DiabeticCondition {
 		System.out.println("<-- Please Enter your Hemoglobin A1C value below -->");
 		Scanner scanner = new Scanner(System.in);
 		double hbga1c = scanner.nextDouble();
-		if (hbga1c > 6.4) {
-			System.out.println("I am a diabetic patient");
-		} else {
-			if (hbga1c == 6.4) {
-				System.out.println("Congratulation! you are not diabetic");
-			} else if (hbga1c < 6.4 && hbga1c >= 5.7) {
-				System.out.println("I am a pre-diabetic patient");
-			} else if (hbga1c < 5.7)
+		if(hbga1c>6.4) {
+			System.out.println("i am a diabatic patient");
+		}else {
+			if(hbga1c>=5.7) {
+				System.out.println("I am a pre-diabatic patient ");
+			}else if(hbga1c<5.7) {
 				System.out.println("I am a healthy person");
-		}
-		scanner.close();
-	}
-}
+			}
+		}scanner.close();
+		
+  }
+ }
